50b0300a08a36a15a59a0030e9fd581e3fa8a220
Varshini R <156539614+varshinir08@users.noreply.github.com>
2025-06-18 23:11:23 +0530

    Add files via upload

---
+++ EX-1/Logger.java
@@ -0,0 +1,16 @@
+
+public class Logger { 
+    private static Logger instance; 
+    private Logger() {
+        System.out.println("Logger initialized");
+    } 
+    public static synchronized Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();   
+        }
+        return instance;
+    } 
+    public void log(String message) {
+        System.out.println("Log: " + message);
+    }
+}
