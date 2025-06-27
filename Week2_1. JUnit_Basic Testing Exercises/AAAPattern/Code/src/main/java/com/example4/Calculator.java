a4b0239e0d665b6e7a329d68f4148de23f7fcef3
Varshini R <156539614+varshinir08@users.noreply.github.com>
2025-06-27 11:53:46 +0530

    Add files via upload

---
+++ Week2_1. JUnit_Basic Testing Exercises/AAAPattern/Code/src/main/java/com/example4/Calculator.java
@@ -0,0 +1,15 @@
+package com.example4;
+
+public class Calculator {
+	public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public void clearMemory() {
+        System.out.println("Memory cleared!");
+    }
+}
