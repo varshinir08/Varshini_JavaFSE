08801a395af47a2075e4819ac06d4bd8c1ae9658
Varshini R <156539614+varshinir08@users.noreply.github.com>
2025-06-27 12:23:31 +0530

    Add files via upload

---
+++ Week2_TDD using JUnit5 and Mockito/Mocking and Stubbing/Code/src/main/java/com/example1/MyService.java
@@ -0,0 +1,14 @@
+package com.example1;
+
+public class MyService {
+
+    private ExternalApi api;
+
+    public MyService(ExternalApi api) {
+        this.api = api;
+    }
+
+    public String fetchData() {
+        return api.getData();
+    }
+}
