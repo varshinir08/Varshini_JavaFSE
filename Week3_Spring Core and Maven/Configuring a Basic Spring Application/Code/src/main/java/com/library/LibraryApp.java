ebbb3e36c2104140b9c691980df6c1d8f83e02de
Varshini R <156539614+varshinir08@users.noreply.github.com>
2025-07-03 23:24:43 +0530

    Add files via upload

---
+++ Week3_Spring Core and Maven/Configuring a Basic Spring Application/Code/src/main/java/com/library/LibraryApp.java
@@ -0,0 +1,16 @@
+package com.library;
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class LibraryApp {
+	
+	public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+ 
+        BookService bookService = (BookService) context.getBean("bookService");
+ 
+        bookService.addBook("The Great Gatsby");
+    }
+
+}
