34e2348a254146c3c65f3ec96af50e95676b63a8
Varshini R <156539614+varshinir08@users.noreply.github.com>
2025-07-06 21:35:45 +0530

    Add files via upload

---
+++ Week3_Spring Data JPA with Spring Boot, Hibernate/Spring Data JPA - Quick Example/Code/CountryService.java
@@ -0,0 +1,21 @@
+package com.cognizant.service;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.model.Country;
+import com.cognizant.repository.CountryRepository;
+
+import jakarta.transaction.Transactional;
+@Service
+public class CountryService {
+	@Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+
+}
