900401baa9e2686b25d4ea24b6b4739fe8890d57
Varshini R <156539614+varshinir08@users.noreply.github.com>
2025-07-10 22:31:02 +0530

    Add files via upload

---
+++ Week4_Spring REST using Spring Boot 3/Hello World RESTful Web Service/Code/hello-world-rest/src/main/java/com/example/helloworld/HelloWorldRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldRestApplication.class, args);
+	}
+
+}
